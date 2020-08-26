484058c517d09dbf6d27289f650ac1ef853c53b7
Jai Gokhale <gokhale.jaidev@gmail.com>
2020-08-26 15:32:57 -0500

    added bootstrap form and buttons

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,50 @@ const submitFormButton = document.querySelector('#submitForm');
 
 function getQuote(category) {
   const apiUrl = `https://api.chucknorris.io/jokes/random?category=${category}`;
-  const chuckSaysParagraph = document.querySelector('#chuckSays');
-
-  get(apiUrl).then(function(response) {
-    chuckSaysParagraph.innerHTML = response.value;
-  });
+  
+  //jquery API call that replaced the JavaScript approach we previously implemented
+  $.ajax({
+    type: 'GET',
+    url: apiUrl,
+    success: function(response){
+      //jQuery approach to updating DOM
+      $('#chuckSays').text(response.value)
+    },
+    error: function(error){
+      console.error('error:',error)
+    }
+  })
 }
+
+
+
+
+//appending categories to a dropdown menu
+const getCategories = () => {
+  const url = `https://api.chucknorris.io/jokes/categories`;
+  const dropdownMenu = document.getElementById('categoryInput')
+  $.ajax({
+    type: 'GET',
+    url: url,
+    success: function(response){
+      //jQuery approach to updating DOM
+      response.map(function (category) {
+        if (category != "explicit") {
+            const categoryOption = document.createElement('option')
+            categoryOption.value = category;
+            categoryOption.text = category.charAt(0).toUpperCase() + category.slice(1);
+            dropdownMenu.appendChild(categoryOption);
+        }
+    })
+    },
+    error: function(error){
+      console.error('error:',error)
+    }
+  })
+}
+
+
+
 
 refreshQuoteButton.addEventListener('click', function(e) {
   e.preventDefault();
@@ -27,4 +65,11 @@ submitFormButton.addEventListener('click', function(e) {
   getQuote(category);
 });
 
-getQuote(category);
+
+$(document).ready(()=>{
+  getCategories();
+  getQuote(category);
+});
+  
+
+
